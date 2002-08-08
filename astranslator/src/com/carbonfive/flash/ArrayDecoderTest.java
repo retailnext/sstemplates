815b65f626b305df332dcfd85c4fd2f860b336b2
Michael Wynholds <wynholds@sourceforge.net>
2002-08-08 18:36:12 +0000

    unit tests for ArrayEncoder and ArrayDecoder -mike

---
+++ astranslator/src/com/carbonfive/flash/ArrayDecoderTest.java
@@ -0,0 +1,81 @@
+package com.carbonfive.flash;
+
+import java.util.*;
+import junit.framework.*;
+
+public class ArrayDecoderTest
+  extends    TestCase
+{
+  public ArrayDecoderTest(String name)
+  {
+    super(name);
+  }
+
+  public static Test suite()
+  {
+    TestSuite suite = new TestSuite(ArrayDecoderTest.class);
+    return suite;
+  }
+
+  private DecoderFactory factory = null;
+
+  protected void setUp()
+    throws Exception
+  {
+    factory =  DecoderFactory.getInstance();
+  }
+
+  protected void tearDown()
+    throws Exception
+  {
+    factory = null;
+  }
+
+  public void testArrayListToObjectArrayDecode()
+  {
+    ArrayList list = new ArrayList();
+    Object obj1 = new Object();
+    Object obj2 = new Object();
+    list.add(obj1);
+    list.add(obj2);
+
+    Class arrayClass = new Object[2].getClass();
+
+    ActionScriptDecoder decoder = factory.getDecoder(list, arrayClass);
+    assertNotNull(decoder);
+
+    Object decodedObject = decoder.decodeObject(list, arrayClass);
+    assertTrue(decodedObject instanceof Object[]);
+
+    Object[] array = (Object[]) decodedObject;
+    assertNotNull(array);
+    assertEquals(2, array.length);
+    assertEquals(obj1, array[0]);
+    assertEquals(obj2, array[1]);
+  }
+
+  public void testArrayListToIntegerArrayDecode()
+  {
+    ArrayList list = new ArrayList();
+    Integer int1 = new Integer(1);
+    Integer int2 = new Integer(2);
+    Double  dbl1 = new Double(int1.intValue());
+    Double  dbl2 = new Double(int2.intValue());
+    list.add(dbl1);
+    list.add(dbl2);
+
+    Class arrayClass = new Integer[2].getClass();
+
+    ActionScriptDecoder decoder = factory.getDecoder(list, arrayClass);
+    assertNotNull(decoder);
+
+    Object decodedObject = decoder.decodeObject(list, arrayClass);
+    assertTrue(decodedObject instanceof Integer[]);
+
+    Integer[] array = (Integer[]) decodedObject;
+    assertNotNull(array);
+    assertEquals(2, array.length);
+    assertEquals(int1, array[0]);
+    assertEquals(int2, array[1]);
+  }
+}
---
+++ astranslator/src/com/carbonfive/flash/ArrayEncoderTest.java
@@ -0,0 +1,75 @@
+package com.carbonfive.flash;
+
+import java.util.*;
+import junit.framework.*;
+
+public class ArrayEncoderTest
+  extends    TestCase
+{
+  public ArrayEncoderTest(String name)
+  {
+    super(name);
+  }
+
+  public static Test suite()
+  {
+    TestSuite suite = new TestSuite(ArrayEncoderTest.class);
+    return suite;
+  }
+
+  private EncoderFactory factory = null;
+
+  protected void setUp()
+    throws Exception
+  {
+    factory =  EncoderFactory.getInstance();
+  }
+
+  protected void tearDown()
+    throws Exception
+  {
+    factory = null;
+  }
+
+  public void testObjectArrayToArrayListEncode()
+  {
+    Object obj1 = new Object();
+    Object obj2 = new Object();
+    Object[] array = new Object[] { obj1, obj2 };
+
+    ActionScriptEncoder encoder = factory.getEncoder(array);
+    assertNotNull(encoder);
+
+    Object encodedObject = encoder.encodeObject(array);
+    assertNotNull(encodedObject);
+    assertTrue(encodedObject instanceof ArrayList);
+
+    ArrayList list = (ArrayList) encodedObject;
+    assertEquals(2, list.size());
+    assertEquals(obj1, list.get(0));
+    assertEquals(obj2, list.get(1));
+  }
+
+  public void testIntegerArrayToArrayListEncode()
+  {
+    Integer int1 = new Integer(1);
+    Integer int2 = new Integer(2);
+    Integer[] array = new Integer[] { int1, int2 };
+    Double  dbl1 = new Double(int1.intValue());
+    Double  dbl2 = new Double(int2.intValue());
+
+    ActionScriptEncoder encoder = factory.getEncoder(array);
+    assertNotNull(encoder);
+
+    Object encodedObject = encoder.encodeObject(array);
+    assertNotNull(encodedObject);
+    assertTrue(encodedObject instanceof ArrayList);
+
+    ArrayList list = (ArrayList) encodedObject;
+    assertEquals(2, list.size());
+    assertTrue(list.get(0) instanceof Double);
+    assertTrue(list.get(1) instanceof Double);
+    assertEquals(dbl1, list.get(0));
+    assertEquals(dbl2, list.get(1));
+  }
+}
