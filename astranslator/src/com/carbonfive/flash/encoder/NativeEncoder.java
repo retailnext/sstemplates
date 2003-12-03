47ac153d1bdea6ed875b7ad3da4f2c36c07b3941
Michael Wynholds <wynholds@sourceforge.net>
2003-12-03 01:30:14 +0000

    never send down anything that extends Date because it won't matter to Flash, and it might have extra information that wastes space. -mike

--- astranslator/src/com/carbonfive/flash/encoder/NativeEncoder.java
+++ astranslator/src/com/carbonfive/flash/encoder/NativeEncoder.java
@@ -1,6 +1,8 @@
 package com.carbonfive.flash.encoder;
 
+import java.util.*;
 import com.carbonfive.flash.*;
+import org.apache.commons.logging.*;
 
 /**
  * Encodes a Java native object to an ActionScript native object.
@@ -8,13 +10,28 @@ import com.carbonfive.flash.*;
 public class NativeEncoder
   extends ActionScriptEncoder
 {
+  private static final Log log = LogFactory.getLog(NativeEncoder.class);
+
   public Object encodeShell(Context ctx, Object decodedObject)
   {
+    if (decodedObject == null) return null;
+
+    if ((decodedObject instanceof Date) && ! decodedObject.getClass().equals(Date.class))
+    {
+      return new Date(((Date) decodedObject).getTime());
+    }
+    
     return decodedObject;
   }
 
+  /**
+   * Passes decodedObject straight through.  The only exception is that this method
+   * transforms any object that extends Date into a regular Date object, as that is
+   * all that Flash can handle.  Actually, passing the extended object will work but
+   * is wasteful.
+   */
   public Object encodeObject(Context ctx, Object shell, Object decodedObject)
   {
-    return decodedObject;
+    return shell;
   }
 }
--- astranslator/src/com/carbonfive/flash/encoder/NativeEncoderTest.java
+++ astranslator/src/com/carbonfive/flash/encoder/NativeEncoderTest.java
@@ -37,19 +37,24 @@ public class NativeEncoderTest
   {
     NativeEncoder encoder = new NativeEncoder();
 
-    Date    date   = new Date();
-    String  string = "hello";
-    Boolean bool   = Boolean.TRUE;
+    Date    date    = new Date();
+    Date    sqlDate = new java.sql.Date(date.getTime());
+    String  string  = "hello";
+    Boolean bool    = Boolean.TRUE;
 
-    assertTrue(encoder.encodeObject(Context.getBaseContext(), date)   instanceof Date);
-    assertTrue(encoder.encodeObject(Context.getBaseContext(), string) instanceof String);
-    assertTrue(encoder.encodeObject(Context.getBaseContext(), bool)   instanceof Boolean);
+    assertTrue(encoder.encodeObject(Context.getBaseContext(), date)    instanceof Date);
+    assertTrue(encoder.encodeObject(Context.getBaseContext(), sqlDate) instanceof Date);
+    assertTrue(encoder.encodeObject(Context.getBaseContext(), string)  instanceof String);
+    assertTrue(encoder.encodeObject(Context.getBaseContext(), bool)    instanceof Boolean);
 
-    Date    encodedDate   = (Date)    encoder.encodeObject(Context.getBaseContext(), date);
-    String  encodedString = (String)  encoder.encodeObject(Context.getBaseContext(), string);
-    Boolean encodedBool   = (Boolean) encoder.encodeObject(Context.getBaseContext(), bool);
+    Date    encodedDate    = (Date)    encoder.encodeObject(Context.getBaseContext(), date);
+    Date    encodedSqlDate = (Date)    encoder.encodeObject(Context.getBaseContext(), sqlDate);
+    String  encodedString  = (String)  encoder.encodeObject(Context.getBaseContext(), string);
+    Boolean encodedBool    = (Boolean) encoder.encodeObject(Context.getBaseContext(), bool);
 
     assertEquals(date,   encodedDate);
+    assertEquals(date,   encodedSqlDate); // should be a java.lang.Date, not a java.sql.Date
+    assertEquals(Date.class, encodedSqlDate.getClass());
     assertEquals(string, encodedString);
     assertEquals(bool,   encodedBool);
   }
