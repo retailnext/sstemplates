b0e2a729827e7963056ee1df9abf637e572f002e
Michael Wynholds <wynholds@sourceforge.net>
2008-02-21 23:46:28 +0000

    test for Coordinates. -mike

---
+++ sstemplates/src/com/carbonfive/sstemplates/CoordinatesTest.java
@@ -0,0 +1,27 @@
+package com.carbonfive.sstemplates;
+
+import junit.framework.TestCase;
+import org.apache.commons.logging.*;
+
+import java.io.Serializable;
+
+public class CoordinatesTest extends TestCase
+  implements Serializable
+{
+  private static final Log log = LogFactory.getLog(CoordinatesTest.class);
+
+  public void testToString() throws Exception
+  {
+    Coordinates c = new Coordinates(0, (short) 0);
+    assertEquals("A1", c.toString());
+
+    c = new Coordinates(1, (short) 1);
+    assertEquals("B2", c.toString());
+
+    c = new Coordinates(2, (short) 26);
+    assertEquals("AA3", c.toString());
+
+    c = new Coordinates(3, (short) 53);
+    assertEquals("BB4", c.toString());
+  }
+}
